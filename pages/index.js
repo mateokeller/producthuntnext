5f24610e140fae52911fc4f8c7e4b4e5a3f9a929
mateokeller <mateokellergms@gmail.com>
2023-01-12 10:27:21 -0300

    fix product list

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,50 @@
-import Layout from '../components/layout/Layout';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState, useContext } from "react";
+import Layout from "../components/layout/Layout";
+import ProductDetail from "../components/layout/ProductDetail";
 
-const Home = () => (
+import { FirebaseContext } from "../firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtainProducts = () => {
+      const querySnapshot = collection(firebase.db, "products");
+      onSnapshot(querySnapshot, ({ docs }) => {
+        const newProducts = docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+
+          return {
+            ...data,
+            id,
+          };
+        });
+        setProducts(newProducts);
+      });
+    };
+    obtainProducts();
+  }, []);
+
+  return (
     <div>
-        <Layout>
-            <h1>Inicio</h1>
-        </Layout>
+      <Layout>
+        <div className="product-list">
+          <div className="products-container">
+            <ul className="bg-white">
+              {products.map((product, id) => (
+                <ProductDetail key={product.id} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
     </div>
-)
+  );
+};
 
 export default Home;
