75f5e367fadcae7c3520d79145e98a84933c1771
mateokeller <mateokellergms@gmail.com>
2023-01-24 10:35:02 -0300

    fix next config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// import { Model } from "firebase-admin/lib/machine-learning/machine-learning";
+// import next from "next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+};
+
+// module.exports = {
+//   images: {
+//     domains: ["localhost"],
+//   },
+// };
+
+module.exports = nextConfig;
